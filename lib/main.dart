9fce0828e6d4b6fc4b973807e32b3fc80a1da3e9
Evelyn Becker <evelyn.becker@proway.treina>
2025-05-20 11:52:51 -0300

    commit inicial

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Container(
+    child: Text(
+      "Bem vindo ao Flutter", 
+      textDirection: TextDirection.ltr,
+      style: TextStyle(color: const Color.fromARGB(255, 231, 46, 231), fontSize: 30.0),
+    ),
+  ));
+}
+
+
+
+
+
